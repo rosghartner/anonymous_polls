f62dffc147a305cb18236077ce47f48c8ecdcdcd
rosghartner <rosghartner@gmail.com>
2021-07-23 19:27:41 +0300

    added unit tests in test mode

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,88 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from .models import Poll
+
+
+
+class PollsTests(APITestCase):
+    """Тестирование"""
+
+    @classmethod
+    def setUpTestData(cls):
+
+        user_test1 = User.objects.create_user(username='test', password='use12345')
+        user_test1.save()
+        cls.user_test1_token = Token.objects.create(user=user_test1)
+        cls.poll_data = {
+            "title": "test1_poll_title",
+            "creator": 1,
+            "description": "test1_poll_description",
+            "questions": [
+                {
+                "question": "test1_question1",
+                "answers": [
+                    {
+                    "answer": "test1_answer1"
+                    },
+                    {
+                    "answer": "test1_answer2"
+                    }
+                ]
+                },
+                {
+                "question": "test1_question2",
+                "answers": [
+                    {
+                    "answer": "test1_answer3"
+                    },
+                    {
+                    "answer": "test1_answer4"
+                    }
+                ]
+                }
+            ]
+        }
+        cls.one_poll = Poll.objects.create(
+            creator = user_test1,
+            title = 'test',
+            description = 'description',
+        )
+
+    def setUp(self):
+        # user_test1 = User.objects.create_user(username='test', password='use12345')
+        # user_test1.save()
+        # self.user_test1_token = Token.objects.create(user=user_test1)
+        pass
+
+    def test_poll_list_invalid_auth(self):
+        response = self.client.get(reverse('poll_list'))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_poll_list_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.get(reverse('poll_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_create_poll_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.post(reverse('create_poll'), self.poll_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+    def test_create_poll_invalid_auth(self):
+        response = self.client.post(reverse('create_poll'), self.poll_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_poll_id_invalid_auth(self):
+        response = self.client.get(reverse('poll_id', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_poll_id_valid(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_test1_token.key)
+        response = self.client.get(reverse('poll_id', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(f'************{response.data}***********')
--- polls/urls.py
+++ polls/urls.py
@@ -3,25 +3,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path('rating/', views.AddRatingViewSet.as_view({'post': 'create'}), name=''),
+    path('poll-list/', views.PollsViewSet.as_view({'get': 'list'}), name='poll_list'),
+    path('poll/<int:pk>', views.PollsViewSet.as_view({'get': 'retrieve'}), name='poll_id'),
+    path('create-poll/', views.PollsViewSet.as_view({'post': 'create'}), name='create_poll'),
+    path('update-poll/<int:pk>', views.PollsViewSet.as_view({'post': 'update'})),
+    path('delete-poll/<int:pk>', views.PollsViewSet.as_view({'post': 'destroy'})),
+    path('create-question/', views.QuestionsViewSet.as_view({'post': 'create'})),
+    path('update-question/<int:pk>', views.QuestionsViewSet.as_view({'post': 'update'})),
+    path('delete-question/<int:pk>', views.QuestionsViewSet.as_view({'post': 'destroy'})),
+    path('create-answer/', views.AnswersViewSet.as_view({'post': 'create'})),
+    path('update-answer/<int:pk>', views.AnswersViewSet.as_view({'post': 'update'})),
+    path('delete-answer/<int:pk>', views.AnswersViewSet.as_view({'post': 'destroy'})),
     path('poll/choice', views.CreateChoiceViewSet.as_view({'post': 'create'})),
-    #path('poll/update/<int:pk>', views.UpdatePollView.as_view()),
-
-    path('rating/', views.AddRatingViewSet.as_view({'post': 'create'})), #works
-    path('poll-list/', views.PollsViewSet.as_view({'get': 'list'})), #works
-    path('poll/<int:pk>', views.PollsViewSet.as_view({'get': 'retrieve'})), #works
-    path('create-poll/', views.PollsViewSet.as_view({'post': 'create'})), #works
-    path('update-poll/<int:pk>', views.PollsViewSet.as_view({'post': 'update'})), #works
-    path('delete-poll/<int:pk>', views.PollsViewSet.as_view({'post': 'destroy'})), #works
-    path('create-question/', views.QuestionsViewSet.as_view({'post': 'create'})), #works
-    path('update-question/<int:pk>', views.QuestionsViewSet.as_view({'post': 'update'})), #works
-    path('delete-question/<int:pk>', views.QuestionsViewSet.as_view({'post': 'destroy'})), #works
-    path('create-answer/', views.AnswersViewSet.as_view({'post': 'create'})), #works
-    path('update-answer/<int:pk>', views.AnswersViewSet.as_view({'post': 'update'})), #works
-    path('delete-answer/<int:pk>', views.AnswersViewSet.as_view({'post': 'destroy'})), #works
-    path('poll/user-created', views.PollDataViewSet.as_view({'get': 'list'})), #works
-    path('poll/data/<int:pk>', views.PollDataViewSet.as_view({'get': 'retrieve'})), #works
-    path('user/polls/', views.ResultUserPollsViewSet.as_view({'get': 'list'})), #works
-    path('user/polls/<int:pk>', views.ResultUserPollsViewSet.as_view({'get': 'retrieve'})), #works
+    path('poll/user-created', views.PollDataViewSet.as_view({'get': 'list'})),
+    path('poll/data/<int:pk>', views.PollDataViewSet.as_view({'get': 'retrieve'})),
+    path('user/polls/', views.ResultUserPollsViewSet.as_view({'get': 'list'})),
+    path('user/polls/<int:pk>', views.ResultUserPollsViewSet.as_view({'get': 'retrieve'})),
 
 
 ]
